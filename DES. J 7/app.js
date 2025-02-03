72521978f7219f131d3d87253348301c41a155a9
Juliana Rodrigues <sousa.juli1234@gmail.com>
2025-02-03 14:29:12 -0300

    Projeto J 7

---
+++ DES. J 7/app.js
@@ -0,0 +1,13 @@
+let listaGenerica = [];
+let linguagemDeProgramação = ['java, javaScript, phyton, css'];
+linguagemDeProgramação.push['java, phyton, css']
+let listaNomes = ['java, phuton, css'];
+console.log('nomes[0]');
+Nomes = ['java, phyton, css'];
+console.log('nomes[1]');
+Nomes = ['phyton, java, javaScript'];
+console.log('nomes[2]');
+
+
+
+
